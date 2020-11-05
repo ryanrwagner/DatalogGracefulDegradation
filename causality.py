815cf990a6478ce7c3c6f3ee0e2d5175f4330a43
Ryan Wagner <rrwagner@cs.cmu.edu>
2020-11-04 19:35:16 -0500

    Beginning to add causal stuff. Prior to cleanup

--- causality.py
+++ causality.py
@@ -1,5 +1,18 @@
-transformerFails(X) <= compromised(X) & isType(X,'transformer')
-transformerFails(X) <= relayFails(Y)
-shortTermPowerOutage() <= generatorFails(X)
-longTermPowerOutage() <= transformerFails(X)
+transformerFails(X,P2) <= compromised(X,P) & isType(X,'transformer')
+transformerFails(X,P2) <= relayFails(Y,P)
+shortTermPowerOutage(P2) <= generatorFails(X,P)
+longTermPowerOutage(P2) <= transformerFails(X,P)
+
+
+#Causal style for 
+#Full backup
+
+#Alternate backup (PACE)
+
+#No backup
+
+#Ands / Ors
+
+#Step degradation / Partial backup
+
 
---
+++ subsystem-module-example.py
@@ -0,0 +1,3 @@
+# External-facing components
+# Traces and data flow impacts
+# Functional impacts may be on the consumer or producer side, so they're emergent properties that can't be quantified here unless there's a consumer internal to the subsystem.
